4ac76169fd6dbd3f0a2c93b1b35a0309abcc5fca
mdelacua <marcos.delacuadra95@gmail.com>
2023-04-13 20:13:57 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: BienvenidoComponent},
+   {path: 'bienvenido', component: BienvenidoComponent},
+   {path: 'error', component: ErrorComponent},
+   {path: 'login', component: LoginComponent},
+   {path: '**', component: ErrorComponent}//el default siempre deberia ir al final porque funciona como un if, si se cumple la condicion se ejecuta primero
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
